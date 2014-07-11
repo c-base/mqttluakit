b9bdb3263800c042d67fbddd7f325b40cd689b94
cmile <github@cmile.de>
2014-07-11 19:18:37 +0200

    moved urls to seperate file

--- mqttluakit.py
+++ mqttluakit.py
@@ -5,23 +5,25 @@ import time
 import os
 from datetime import datetime, timedelta
 from random import choice
+from urls import urls
 
 mqtt_client_id = "nerdctrl-luakit"
 mqtt_server = "c-beam.cbrp3.c-base.org"
 page_timeout = 120
 
-urls = ["http://www.c-base.org", "http://logbuch.c-base.org/", "http://c-portal.c-base.org", 
-    "http://c-beam.cbrp3.c-base.org/events", "https://c-beam.cbrp3.c-base.org/c-base-map",
-    "http://cbag3.c-base.org/artefact", "https://c-beam.cbrp3.c-base.org/missions", "https://c-beam.cbrp3.c-base.org/weather",
-    "http://c-beam.cbrp3.c-base.org/bvg", "http://c-beam.cbrp3.c-base.org/nerdctrl",
-    "https://c-beam.cbrp3.c-base.org/rickshaw/examples/fixed.html",
-    "https://c-beam.cbrp3.c-base.org/sensors",
-    "https://c-beam.cbrp3.c-base.org/ceitloch",
-    "http://visibletweets.com/#query=@cbase&animation=2",
-    "https://c-beam.cbrp3.c-base.org/reddit",
-    "http://vimeo.com/cbase/videos",
-    "https://wiki.cbrp3.c-base.org/dokuwiki/",
-]
+
+#urls = ["http://www.c-base.org", "http://logbuch.c-base.org/", "http://c-portal.c-base.org", 
+    #"http://c-beam.cbrp3.c-base.org/events", "https://c-beam.cbrp3.c-base.org/c-base-map",
+    #"http://cbag3.c-base.org/artefact", "https://c-beam.cbrp3.c-base.org/missions", "https://c-beam.cbrp3.c-base.org/weather",
+    #"http://c-beam.cbrp3.c-base.org/bvg", "http://c-beam.cbrp3.c-base.org/nerdctrl",
+    #"https://c-beam.cbrp3.c-base.org/rickshaw/examples/fixed.html",
+    #"https://c-beam.cbrp3.c-base.org/sensors",
+    #"https://c-beam.cbrp3.c-base.org/ceitloch",
+    #"http://visibletweets.com/#query=@cbase&animation=2",
+    #"https://c-beam.cbrp3.c-base.org/reddit",
+    #"http://vimeo.com/cbase/videos",
+    #"https://wiki.cbrp3.c-base.org/dokuwiki/",
+#]
 
 last_change = datetime.now()
 
